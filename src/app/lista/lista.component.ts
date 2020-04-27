fa2fabeeeef98ca48941e7f6d3df3a0192dc3c9c
ToniPigolotti <pigolottiantonino@gmail.com>
2020-04-27 09:50:38 +0200

    modifiche

--- src/app/lista/lista.component.ts
+++ src/app/lista/lista.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-lista',
   templateUrl: './lista.component.html',
-  styleUrls: ['./lista.component.scss']
+  styleUrls: ['./lista.component.css']
 })
 export class ListaComponent implements OnInit {
 
